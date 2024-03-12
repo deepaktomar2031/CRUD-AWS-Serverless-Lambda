6004a7b16c77ce5211f4ec372ceeb6cc815c9d36
deepaktomar2031 <deepaktomar2031@gmail.com>
2024-03-12 21:27:19 +0100

    Add createuser function through post request

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const {
   deleteUserById,
 } = require("./src/functions.js");
 
+// Read all users
 module.exports.readAllUsers = async (event, context, callback) => {
   const { success, data } = await readAllUsers();
 
@@ -23,6 +24,7 @@ module.exports.readAllUsers = async (event, context, callback) => {
   };
 };
 
+// Read Users by ID
 module.exports.getUserById = async (event, context, callback) => {
   const id = event.pathParameters.id;
 
@@ -42,10 +44,33 @@ module.exports.getUserById = async (event, context, callback) => {
     body: JSON.stringify({ success: false, message: "Error" }),
   };
 };
+
+// Delete User by ID
 module.exports.deleteUserById = async (event, context, callback) => {
   const id = event.pathParameters.id;
 
   const { success, data } = await deleteUserById(id);
+  if (success) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ success, data }),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    };
+  }
+
+  return {
+    statusCode: 500,
+    body: JSON.stringify({ success: false, message: "Error" }),
+  };
+};
+
+// Create User
+module.exports.createOrUpdate = async (event, context, callback) => {
+  const requestBody = JSON.parse(event.body);
+
+  const { success, data } = await createOrUpdate(requestBody);
   if (success) {
     return {
       statusCode: 200,
